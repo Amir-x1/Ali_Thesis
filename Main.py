f34c0fd22e2b12d12ecfece79e28d02b88e8b7fa
Amir <zendehboudi.amir@gmail.com>
2023-01-11 12:51:09 +0330

    distance to sink

--- Main.py
+++ Main.py
@@ -1,26 +1,33 @@
 from MyFunctions import *
+from pathlib import Path
 import json
 
 # ################## User inputs
-
-# Nodes generator ----> 1 = you have nodes   #  0 = you dont have nodes
-NodesGeneratedBefore = 0
-# Enter number of nodes
+# number of nodes
 NodesCount = 100
-# Enter X  and Y of study area
+# Maximum signal range in meters
+MaxSignalDistance = 15
+# Distance calculation error
+error = 0.01
+# X  and Y of study area in meters
 X = 100
 Y = 100
-# Enter the position of Sink node
+# the position of Sink node
 SinkX = 50
 SinkY = 50
-# Enter the position of first known node
+# position of first known node
 FnodeX = 54
 FnodeY = 54
-# Enter the position of second known node
+# position of second known node
 SnodeX = 46
 SnodeY = 46
 # ################################ Main ##################################
+# ########## This part generates nodes and save them in a json file
+# check if the file generated before
 file_name = '%i Nodes for (%i)in(%i) Area.json' % (NodesCount, X, Y)
+my_file = Path(file_name)
+NodesGeneratedBefore = my_file.exists()
+# ##########
 if not NodesGeneratedBefore:
     Nodes = {
         0: {"x": SinkX, "y": SinkY},
@@ -33,5 +40,60 @@ if not NodesGeneratedBefore:
     json_file = json.dumps(Nodes, indent=2)
     with open(file_name, 'w') as f:
         f.write(json_file)
-
+else:
+    with open(file_name, 'r') as h:
+        Nodes = json.loads(h.read())
+        # for node in Nodes:
+        # node = int(node)
+        # print(Nodes[node]['x'])
 # print(Nodes)
+# #########################################################################
+# Calculate distance of each node to sink
+distanceToSink = []
+Sink = [SinkX, SinkY]
+for i in Nodes:
+    node = [Nodes[i]['x'], Nodes[i]['y']]
+    distanceToSink.append(distance(Sink, node, error))
+# print(distanceToSink)
+# ###########################################################
+# Start sending signals
+calculatedPositions = []
+for i in range(NodesCount + 1):
+    calculatedPositions.append([0, 0])
+joints = []
+for i in range(NodesCount + 1):
+    joints.append([0, 0])
+knownNodes = []
+for i in range(NodesCount + 1):
+    knownNodes.append(0)
+knownNodes[0] = 1
+knownNodes[1] = 1
+knownNodes[2] = 1
+# print(len(knownNodes))
+# print(len(Nodes))
+# print((knownNodes))
+for i in Nodes:
+    if knownNodes[int(i)] == 0:
+        firstNode = [Nodes[i]['x'], Nodes[i]['y']]
+        for j in Nodes:
+            if knownNodes[int(j)] == 1:
+                secondNode = [Nodes[j]['x'], Nodes[j]['y']]
+                r = receiver(firstNode, secondNode, MaxSignalDistance, error)
+                if r and joints[int(i)][0] == 0:
+                    joints[int(i)][0] = int(j)
+                elif r and joints[int(i)][1] == 0:
+                    joints[int(i)][1] = int(j)
+for i in Nodes:
+    if joints[int(i)][0] != 0 and joints[int(i)][1] != 0:
+        knownNodes[int(i)] = 1
+        fN = [Nodes[str(joints[int(i)][0])]['x'], Nodes[str(joints[int(i)][0])]['y']]
+        sN = [Nodes[str(joints[int(i)][1])]['x'], Nodes[str(joints[int(i)][1])]['y']]
+        # print(str(joints[int(i)][1]))
+        # print(joints[int(i)])
+        # print(fN)
+        # print(sN)
+        # print(Sink)
+        calculatedPositions[int(i)] = position_calculator(fN, sN, Sink, error)
+
+# print(joints)
+# print(knownNodes)
--- MyFunctions.py
+++ MyFunctions.py
@@ -1,7 +1,52 @@
 import random
+import math
 
 
 def node_generator(x, y):
     x = random.randint(0, x)
     y = random.randint(0, y)
     return {"x": x, "y": y}
+
+
+def distance(first_node, second_node, error):
+    e = error * random.randint(-100, 100) / 100
+    e = e + 1
+    d = e * math.sqrt((first_node[0] - second_node[0]) ** 2 + (first_node[1] - second_node[1]) ** 2)
+    return d
+
+
+def receiver(first_node, second_node, max_signal_distance, error):
+    d = distance(first_node, second_node, error)
+    if d <= max_signal_distance:
+        return d
+    else:
+        return 0
+
+
+def position_calculator(first_node, second_node, sink, error):
+    x1 = first_node[0]
+    y1 = first_node[1]
+    x2 = second_node[0]
+    y2 = second_node[1]
+    x3 = sink[0]
+    y3 = sink[1]
+
+    x12 = first_node[0] ** 2
+    y12 = first_node[1] ** 2
+    x22 = second_node[0] ** 2
+    y22 = second_node[1] ** 2
+    x32 = sink[0]**2
+    y32 = sink[1]**2
+
+    print(x1)
+    print(x2)
+    print(x3)
+    print(y1)
+    print(y2)
+    print(y3)
+
+    a = -(-x12*y2 + x12*y3 + x22*y1 - x22*y3 - x32*y1 + x32*y2 - y12*y2 + y12*y3 + y1*y22 - y1*y32 - y22*y3 + y32*y2) \
+        / (2*x1*y2 - 2*x1*y3 - 2*x2*y1 - 2*x2*y3 - 2*x3*y1 - 2*x3*y2)
+    b = -(x12*x2 - x12*x3 - x1*x22 + x1*x32 - x1*y22 + x1*y32 + x3*x22 - x32*x2 + y12*x2 - x2*y32 - y12*x3 + x3*y22)\
+        / (2*x1*y2 - 2*x1*y3 - 2*x2*y1 + 2*x2*y3 + 2*x3*y1 - 2*x3*y2)
+    return [a, b]
