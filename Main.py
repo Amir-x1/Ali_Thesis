e3ff3b5c02815c7e365af9a801844dba0366d8a9
Amir <zendehboudi.amir@gmail.com>
2023-01-16 09:58:42 +0330

    Works cool with error=0

--- Main.py
+++ Main.py
@@ -1,14 +1,15 @@
 from MyFunctions import *
 from pathlib import Path
 import json
+import matplotlib.pyplot as plt
 
 # ################## User inputs
 # number of nodes
-NodesCount = 100
+NodesCount = 500
 # Maximum signal range in meters
-MaxSignalDistance = 15
+MaxSignalDistance = 25
 # Distance calculation error
-error = 0
+error = 0.000
 # X  and Y of study area in meters
 X = 100
 Y = 100
@@ -28,6 +29,7 @@ file_name = '%i Nodes for (%i)in(%i) Area.json' % (NodesCount, X, Y)
 my_file = Path(file_name)
 NodesGeneratedBefore = my_file.exists()
 # ##########
+Sink = [SinkX, SinkY]
 if not NodesGeneratedBefore:
     Nodes = {
         0: {"x": SinkX, "y": SinkY},
@@ -36,10 +38,16 @@ if not NodesGeneratedBefore:
     }
     for i in range(3, NodesCount + 1):
         newNode = {i: node_generator(X, Y)}
+        if newNode == Sink:
+            print(newNode)
+            newNode = {i: node_generator(X, Y)}
+            print(newNode)
         Nodes = {**Nodes, **newNode}
     json_file = json.dumps(Nodes, indent=2)
     with open(file_name, 'w') as f:
         f.write(json_file)
+    with open(file_name, 'r') as h:
+        Nodes = json.loads(h.read())
 else:
     with open(file_name, 'r') as h:
         Nodes = json.loads(h.read())
@@ -50,19 +58,21 @@ else:
 # #########################################################################
 # Calculate distance of each node to sink
 distanceToSink = []
-Sink = [SinkX, SinkY]
 for i in Nodes:
     node = [Nodes[i]['x'], Nodes[i]['y']]
     distanceToSink.append(distance(Sink, node, error))
 # print(distanceToSink)
 # ###########################################################
 # Start sending signals
-calculatedPositions = []
+calculatedLocations = []
 for i in range(NodesCount + 1):
-    calculatedPositions.append([0, 0])
-joints = []
+    calculatedLocations.append([0, 0])
+anchors = []
 for i in range(NodesCount + 1):
-    joints.append([0, 0])
+    anchors.append([0, 0])
+allInOneStraightLine = []
+for i in range(NodesCount + 1):
+    allInOneStraightLine.append(0)
 knownNodes = []
 for i in range(NodesCount + 1):
     knownNodes.append(0)
@@ -72,34 +82,112 @@ knownNodes[2] = 1
 # print(len(knownNodes))
 # print(len(Nodes))
 # print((knownNodes))
-for i in Nodes:
-    if knownNodes[int(i)] == 0:
-        firstNode = [Nodes[i]['x'], Nodes[i]['y']]
-        for j in Nodes:
-            if knownNodes[int(j)] == 1:
-                secondNode = [Nodes[j]['x'], Nodes[j]['y']]
-                r = receiver(firstNode, secondNode, MaxSignalDistance, error)
-                if r and joints[int(i)][0] == 0:
-                    joints[int(i)][0] = int(j)
-                elif r and joints[int(i)][1] == 0:
-                    joints[int(i)][1] = int(j)
-for i in Nodes:
-    if joints[int(i)][0] != 0 and joints[int(i)][1] != 0:
-        knownNodes[int(i)] = 1
-        thisNode = [Nodes[str(i)]['x'], Nodes[str(i)]['y']]
-        fN = [Nodes[str(joints[int(i)][0])]['x'], Nodes[str(joints[int(i)][0])]['y']]
-        sN = [Nodes[str(joints[int(i)][1])]['x'], Nodes[str(joints[int(i)][1])]['y']]
-        # print(str(joints[int(i)][1]))
-        # print(joints[int(i)])
-        # print(fN)
-        # print(sN)
-        # print(Sink)
-        radius1 = distance(fN, thisNode, error)
-        radius2 = distance(sN, thisNode, error)
-        radiusSink = distance(Sink, thisNode, error)
-        calculatedPositions[int(i)] = position_calculator(fN, radius1, sN, radius2, Sink, radiusSink, error)
-        print(i)
-        print(calculatedPositions[int(i)])
+nn = 0
+while sum(knownNodes) <= NodesCount:
+    # Update anchor - find an unknown node and search for a signal that it can receive
+    for i in Nodes:
+        if knownNodes[int(i)] == 0:
+            # print(i)
+            firstNode = [Nodes[i]['x'], Nodes[i]['y']]
+            for j in Nodes:
+                if knownNodes[int(j)] == 1 and anchors[int(i)][0] != int(j):
+                    secondNode = [Nodes[j]['x'], Nodes[j]['y']]
+                    r = receiver(firstNode, secondNode, MaxSignalDistance, error)
+                    if r and anchors[int(i)][0] == 0:
+                        anchors[int(i)][0] = int(j)
+                        # print(joints[int(i)][0])
+                    elif r and anchors[int(i)][1] == 0:
+                        anchors[int(i)][1] = int(j)
+                        # print(joints[int(i)][0])
+                        # print('2 break')
+                        # break
+                    elif r and anchors[int(i)][1] < 0:
+                        # print(joints[int(i)][1])
+                        anchors[int(i)][1] += 1
+
+    # calculate position for nodes which have received two signals
+    for i in Nodes:
+        if knownNodes[int(i)] == 0 and anchors[int(i)][0] != 0 and anchors[int(i)][1] > 0:
+            # print('node ' + i)
+            knownNodes[int(i)] = 1
+            thisNode = [Nodes[i]['x'], Nodes[i]['y']]
+            fN = [Nodes[str(anchors[int(i)][0])]['x'], Nodes[str(anchors[int(i)][0])]['y']]
+            sN = [Nodes[str(anchors[int(i)][1])]['x'], Nodes[str(anchors[int(i)][1])]['y']]
+            radius1 = distance(fN, thisNode, error)
+            radius2 = distance(sN, thisNode, error)
+            radiusSink = distance(Sink, thisNode, error)
+            calculatedLocations[int(i)] = position_calculator(fN, radius1, sN, radius2, Sink, radiusSink, error)
+            # if the node and its two joint nodes are in a straight line function returns None
+            if calculatedLocations[int(i)] is None:
+                # print(calculatedPositions[int(i)])
+                # print(joints[int(i)])
+                print('again ' + i)
+                knownNodes[int(i)] = 0
+                allInOneStraightLine[int(i)] -= 1
+                calculatedLocations[int(i)] = [0, 0]
+                anchors[int(i)][1] = allInOneStraightLine[int(i)]
+                print(calculatedLocations[int(i)])
+                # print(joints[int(i)][1])
+    nn += 1
+    print('Iteration: ' + str(nn))
+    if nn > 100:
+        break
+    # print(i)
+    # print(knownNodes)
+# ###################################################################################
+# print(calculatedLocation)
 # print(joints)
 # print(knownNodes)
-# print(calculatedPositions)
+print(str(sum(knownNodes)-1) + " Nodes has been Found!")
+print(calculatedLocations[190])
+
+ActualLocations = []
+for i in range(NodesCount + 1):
+    ActualLocations.append([0, 0])
+for i in Nodes:
+    ActualLocations[int(i)][0] = Nodes[i]['x']
+    ActualLocations[int(i)][1] = Nodes[i]['y']
+
+
+# x-axis values
+xx = []
+yy = []
+xxA = []
+yyA = []
+for i in range(3, len(calculatedLocations)):
+    xx.append(calculatedLocations[i][0])
+    yy.append(calculatedLocations[i][1])
+    xxA.append(ActualLocations[i][0])
+    yyA.append(ActualLocations[i][1])
+
+
+# plotting points as a scatter plot
+plt.scatter(xxA, yyA, label="Actual Locations", color="black", marker=".", s=30)
+plt.scatter(xx, yy, label="Nodes", color="green", marker=".", s=30)
+plt.scatter(SinkX, SinkY, label="Sink", color="red", marker="*", s=50)
+plt.scatter(FnodeX, FnodeY, label="First known node", color="blue", marker=".", s=30)
+plt.scatter(SnodeX, SnodeY, label="Second known node", color="red", marker=".", s=30)
+
+
+# x-axis label
+plt.xlabel('x - axis')
+# frequency label
+plt.ylabel('y - axis')
+# plot title
+plt.title('All Nodes')
+# showing legend
+# plt.legend()
+plt.grid()
+# function to show the plot
+plt.show()
+
+totalError = 0
+
+for i in range(len(ActualLocations)):
+    if ActualLocations[i][0] - calculatedLocations[i][0] == ActualLocations[i][0]:
+        pass
+    else:
+        xError = ActualLocations[i][0] - calculatedLocations[i][0]
+        yError = ActualLocations[i][1] - calculatedLocations[i][1]
+        totalError += math.sqrt(xError**2 + yError**2)
+print("Total Error is: " + str(totalError))
--- MyFunctions.py
+++ MyFunctions.py
@@ -33,45 +33,68 @@ def intersecting_points_of_two_circle(first_node, r0, second_node, r1):
     d = math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2)
 
     # non intersecting
-    if d > r0 + r1:
-        return None
+    if d >= r0 + r1:
+        return None, None
     # One circle within other
-    if d < abs(r0 - r1):
-        return None
+    if d <= abs(r0 - r1):
+        return None, None
     # coincident circles
     if d == 0 and r0 == r1:
-        return None
+        return None, None
     else:
-        a = (r0 ** 2 - r1 ** 2 + d ** 2) / (2 * d)
-        h = math.sqrt(r0 ** 2 - a ** 2)
-        x2 = x0 + a * (x1 - x0) / d
-        y2 = y0 + a * (y1 - y0) / d
-        x3 = x2 + h * (y1 - y0) / d
-        y3 = y2 - h * (x1 - x0) / d
+        try:
+            a = (r0 ** 2 - r1 ** 2 + d ** 2) / (2 * d)
+            h = math.sqrt(r0 ** 2 - a ** 2)
+            x2 = x0 + a * (x1 - x0) / d
+            y2 = y0 + a * (y1 - y0) / d
+            x3 = x2 + h * (y1 - y0) / d
+            y3 = y2 - h * (x1 - x0) / d
 
-        x4 = x2 - h * (y1 - y0) / d
-        y4 = y2 + h * (x1 - x0) / d
+            x4 = x2 - h * (y1 - y0) / d
+            y4 = y2 + h * (x1 - x0) / d
 
-        return [x3, y3], [x4, y4]
+            return [x3, y3], [x4, y4]
+        except Exception:
+            return None, None
 
 
 def position_calculator(first_node, r0, second_node, r1, sink, rs, error):
-    a, b = intersecting_points_of_two_circle(first_node, r0, second_node, r1)
-    c, d = intersecting_points_of_two_circle(first_node, r0, sink, rs)
-
-    # print(a)
-    # print(b)
-    # print(c)
-    # print(d)
-    if a[0] == c[0] and a[1] == c[1]:
-        print('a1')
+    e = error * random.randint(-100, 100) / 100
+    e = e + 1
+    a, b = intersecting_points_of_two_circle(first_node, r0, sink, rs)
+    c, d = intersecting_points_of_two_circle(second_node, r1, sink, rs)
+    if a is None or b is None:
+        print("None: Changing circles")
+        a, b = intersecting_points_of_two_circle(first_node, r0, second_node, r1)
+    if c is None or d is None:
+        print("None: Changing circles")
+        c, d = intersecting_points_of_two_circle(second_node, r1, first_node, r0)
+    if a is None or b is None or c is None or d is None:
+        return None
+    if abs(a[0] - c[0]) < 0.0001 and abs(a[1] - c[1]) < 0.0001:
+        if a is None or c is None:
+            print("None")
+        a[0] *= e
+        a[1] *= e
         return a
-    elif a[0] == d[0] and a[1] == d[1]:
-        print('a2')
+    elif abs(a[0] - d[0]) < 0.0001 and abs(a[1] - d[1]) < 0.0001:
+        if a is None or d is None:
+            print("None")
+        a[0] *= e
+        a[1] *= e
         return a
-    elif b[0] == c[0] and b[1] == c[1]:
-        print('b1')
+    elif abs(b[0] - c[0]) < 0.0001 and abs(b[1] - c[1]) < 0.0001:
+        if c is None or b is None:
+            print("None")
+        b[0] *= e
+        b[1] *= e
         return b
-    elif b[0] == d[0] and b[1] == d[1]:
-        print('b2')
+    elif abs(b[0] - d[0]) < 0.0001 and abs(b[1] - d[1]) < 0.0001:
+        if d is None or b is None:
+            print("None")
+        b[0] *= e
+        b[1] *= e
         return b
+    else:
+        print("None: No interaction point found")
+        return None
